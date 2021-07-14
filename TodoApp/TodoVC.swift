c106b94d54c2a46b12b379d50bf99e75a70307f1
shivansh <shivanshsinha2598@gmail.com>
2021-07-14 22:37:38 +0530

    New project initialized

---
+++ TodoApp/TodoVC.swift
@@ -0,0 +1,19 @@
+//
+//  TodoVC.swift
+//  TodoApp
+//
+//  Created by Shivansh Sinha on 14/07/21.
+//
+
+import UIKit
+
+class TodoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
