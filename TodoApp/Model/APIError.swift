a614d4de05d8bbc283e904c2a1ff2cadfe75ffd4
shivansh <shivanshsinha2598@gmail.com>
2021-07-15 11:55:16 +0530

    models created

---
+++ TodoApp/Model/APIError.swift
@@ -0,0 +1,12 @@
+//
+//  APIError.swift
+//  TodoApp
+//
+//  Created by Shivansh Sinha on 15/07/21.
+//
+
+import Foundation
+
+struct APIError : Codable {
+    let message : String
+}
---
+++ TodoApp/Model/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  TodoApp
+//
+//  Created by Shivansh Sinha on 15/07/21.
+//
+
+import Foundation
+
+struct Todo : Codable {
+    let item : String
+    let priority : Int
+}
